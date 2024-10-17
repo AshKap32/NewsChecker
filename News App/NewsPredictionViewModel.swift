a649163b9ba2bc55026c1742c6f21fe299fcdbd9
Aashish Kapoor <kapooraashish02@gmail.com>
2024-10-17 13:25:02 -0700

    Switched the API that was being used due to API Issues

---
+++ News App/NewsPredictionViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  NewsPredictionViewModel.swift
+//  News App
+//
+//  Created by Aashish Kapoor on 10/16/24.
+//
+
+import SwiftUI
+
+struct NewsPredictionViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsPredictionViewModel()
+}
